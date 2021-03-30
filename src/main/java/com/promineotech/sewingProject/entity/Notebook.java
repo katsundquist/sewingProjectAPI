27023c2ba5e68ff214678313e56b7686d9cad565
Kat <kat.sundquist@gmail.com>
2021-03-30 13:51:12 -0700

    added CRUD for garment entity

--- src/main/java/com/promineotech/sewingProject/entity/Notebook.java
+++ src/main/java/com/promineotech/sewingProject/entity/Notebook.java
@@ -1,11 +1,14 @@
 package com.promineotech.sewingProject.entity;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -15,6 +18,7 @@ public class Notebook {
 	private Long id;
 	private String name;
 	private String description;
+	private Set<Garment> garments;
 	
 	@JsonIgnore
 	private User user;
@@ -53,6 +57,15 @@ public class Notebook {
 	
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	@OneToMany(mappedBy = "notebook")
+	public Set<Garment> getGarments() {
+		return garments;
+	}
+
+	public void setGarments(Set<Garment> garments) {
+		this.garments = garments;
 	}
 	
 }
